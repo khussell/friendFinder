e7b3d33506241875e56429b9e817dab62e3e7ed6
Lilu <KHussell@gmail.com>
2019-06-07 09:52:51 -0400

    updated people.js array with more keys, updated html for more questions

--- data/people.js
+++ data/people.js
@@ -2,6 +2,15 @@ module.exports = [
     {
      name: "Princess Leia",
      image: "https://img.etimg.com/thumb/msid-59950997,width-643,imgsize-37891,resizemode-4/turns-out-princess-leia-has-a-phd-and-she-got-it-at-19.jpg",
-     first: 3
+     first: 3,
+     second: 2,
+     third: 3,
+     fourth: 5,
+     fifth: 3,
+     sixth: 4,
+     seventh: 3,
+     eigth: 2,
+     ninth: 1,
+     tenth: 3
 }
 ]
